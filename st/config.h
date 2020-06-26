dfadad6cda735b3694e0f764ce45b566a7591ad9
yangp1205 <yangp1205@163.com>
2020-06-26 14:46:53 +0800

    dwm hide tags

--- st/config.h
+++ st/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Monaco:size=14";
-//static char *font = "Fira Code Nerd Font :pixelsize=14:antialias=true:autohint=true";
+//static char *font = "Fira Code Nerd Font :pixelsize=24:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
