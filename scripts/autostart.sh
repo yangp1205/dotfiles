6c8713422776aa8f0dc191f705187347c3af00a2
yangp1205@163.com <john@archlinuxj.localdomain>
2020-09-12 12:18:29 +0800

    add ssh config

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
-/bin/bash ~/scripts/wp-autochange.sh &
+#/bin/bash ~/scripts/wp-autochange.sh &
 /bin/bash ~/scripts/dwm-status.sh &
 picom -b &
 #nm-applet &
